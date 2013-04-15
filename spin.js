d62fd92d72e34e41d47aac702928e105ce97e692
Chris Brown <chris.brown@i-nexus.com>
2013-04-15 20:54:24 +0100

    Initial commit

---
+++ spin.js
@@ -0,0 +1,48 @@
+console.log('test');
+alert('test');
+$('messages').html('test');
+
+var spinning;
+gapi.hangout.data.onStateChanged.add(function(event) {
+  if (spinning != state.spinning) {
+    spinning = state.spinning;
+    if (spinning != null) {
+      disableButton();
+      startSpinning(spinning);
+    } else {
+      enableButton();
+    }
+  }
+});
+
+function onSpin() {
+  var participants = gapi.hangout.getParticipants();
+  var selected = Math.floor(Math.random() * participants.length);
+  gapi.hangout.data.setValue('spinning', selected);
+}
+
+function disableButton() {
+  $('spinButton').attr('disabled', 'disabled');
+}
+
+function enableButton() {
+  $('spinButton').removeAttr('disabled');
+}
+
+function startSpinning(spinning) {
+  var participants = gapi.hangout.getParticipants();
+  var participant = participants[spinning];
+  gapi.hangout.av.setAvatar(participant.id, 'http://4.bp.blogspot.com/_t4ycBBIANiM/SszTB6CmZRI/AAAAAAAACf0/2KFiYY7FydU/s1600/HGG_koi-fishframe.png');
+}
+
+function init() {
+  // When API is ready...                                                         
+  gapi.hangout.onApiReady.add(function(eventObj) {
+    if (eventObj.isApiReady) {
+      enableButton();
+    }
+  });
+}
+
+// Wait for gadget to load.                                                       
+gadgets.util.registerOnLoadHandler(init);
