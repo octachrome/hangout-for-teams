08ab583b58139948da5d5c39c95d72b5a9e02b72
Chris Brown <chris@thebrown.net>
2013-04-16 20:42:19 +0100

    First attempt at spin code

--- spin.js
+++ spin.js
@@ -19,16 +19,52 @@ function onSpin() {
 }
 
 function disableButton() {
-  $('spinButton').attr('disabled', 'disabled');
+  $('#spinButton').attr('disabled', 'disabled');
 }
 
 function enableButton() {
-  $('spinButton').removeAttr('disabled');
+  $('#spinButton').removeAttr('disabled');
 }
 
+var active = [];
+
+function cacheActiveParticipants() {
+  active = [];
+  var participants = gapi.hangout.getParticipants();
+  participants.sort(function(a, b)) {
+    return a.displayIndex - b.displayIndex;
+  });
+  for (var i = 0; i < participants.length; i++) {
+    active.push(participants[i].id);
+  }
+}
+
+var counter = 0;
+var limit = 0;
+
 function startSpinning(spinning) {
-  gapi.hangout.av.setAvatar(spinning, 'http://4.bp.blogspot.com/_t4ycBBIANiM/SszTB6CmZRI/AAAAAAAACf0/2KFiYY7FydU/s1600/HGG_koi-fishframe.png');
-  gapi.hangout.data.setValue('spinning', '');
+  cacheActiveParticipants();
+  var targetIndex = participants.indexOf(spinning);
+  // TODO: adjust # cycles to give a limit of around 20
+  limit = targetIndex + participants.length * 4;
+  counter = 0;
+  updateSpin();
+}
+
+function updateSpin() {
+  if (counter > 0) {
+    var prev = (counter - 1) % active.length;
+    gapi.hangout.av.clearAvatar(active[prev]);
+  }
+
+  var idx = counter % active.length;
+  if (counter < limit) {
+    gapi.hangout.av.setAvatar(active[idx], 'https://hangout-for-teams.googlecode.com/git/spinning.png');
+    setTimeout(updateSpin, 100*(limit-counter+5)/(limit-counter+1));
+  } else {
+    gapi.hangout.av.setAvatar(active[idx], 'https://hangout-for-teams.googlecode.com/git/active.png');
+    gapi.hangout.data.setValue('spinning', '');
+  }
 }
 
 function init() {
